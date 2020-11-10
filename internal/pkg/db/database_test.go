44a66c7924fde0b49c67f67b51e798deec9b205a
Melenium2 <osumelenium@gmail.com>
2020-11-10 15:41:31 +0500

    Added tests for database

--- internal/pkg/db/database_test.go
+++ internal/pkg/db/database_test.go
@@ -1,18 +1,22 @@
 package db_test
 
 import (
+	"Samurai/config"
+	"Samurai/internal/pkg/db"
 	"context"
-	"database/sql"
+	"github.com/jackc/pgx/v4"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 type getter_mock struct {
 }
 
-func (g getter_mock) InsertTx(tx *sql.Tx, ctx context.Context, data interface{}) error {
+func (g getter_mock) Insert(ctx context.Context, data interface{}) (int, error) {
 	panic("implement me")
 }
 
-func (g getter_mock) Insert(ctx context.Context, data interface{}) error {
+func (g getter_mock) InsertTx(tx pgx.Tx, ctx context.Context, data interface{}) (int, error) {
 	panic("implement me")
 }
 
@@ -23,11 +27,100 @@ func (g getter_mock) Get(ctx context.Context, value interface{}) (interface{}, e
 type inserter_mock struct {
 }
 
-func (i inserter_mock) InsertTx(tx *sql.Tx, ctx context.Context, data interface{}) error {
+func (i inserter_mock) Insert(ctx context.Context, data interface{}) (int, error) {
 	panic("implement me")
 }
 
-func (i inserter_mock) Insert(ctx context.Context, data interface{}) error {
+func (i inserter_mock) InsertTx(tx pgx.Tx, ctx context.Context, data interface{}) (int, error) {
 	panic("implement me")
 }
 
+func TestTrackingDatabaseNew_ShouldCreateInstanceByConnAndConfig_NoError(t *testing.T) {
+	c := config.New()
+	repo := db.NewWithConfig(c.Database)
+
+	app := NewApp()
+	id, err := repo.Insert(context.Background(), app)
+	assert.NoError(t, err)
+
+	conn, cleaner := RealDb()
+	defer cleaner("meta_tracking", "app_tracking")
+	repoConn := db.NewWithConnection(conn)
+
+	meta := NewMeta()
+	meta.BundleId = id
+	_, err = repoConn.Insert(context.Background(), meta)
+	assert.NoError(t, err)
+}
+
+func TestTrackingDatabaseInsert_ShouldInsertApp_NoError(t *testing.T) {
+	conn, cleaner := RealDb()
+	defer cleaner("app_tracking")
+
+	app := NewApp()
+	repo := db.New(db.NewAppTracking(conn), inserter_mock{}, inserter_mock{}, inserter_mock{})
+
+	_, err := repo.Insert(context.Background(), app)
+	assert.NoError(t, err)
+}
+
+func TestTrackingDatabaseInsert_ShouldInsertMeta_NoError(t *testing.T) {
+	conn, cleaner := RealDb()
+	defer cleaner("meta_tracking", "app_tracking")
+
+	app := NewApp()
+	meta := NewMeta()
+	repo := db.New(db.NewAppTracking(conn), db.NewMetaTracking(conn), inserter_mock{}, inserter_mock{})
+
+	id, err := repo.Insert(context.Background(), app)
+
+	meta.BundleId = id
+	_, err = repo.Insert(context.Background(), meta)
+	assert.NoError(t, err)
+}
+
+func TestTrackingDatabaseInsert_ShouldInsertKeywordAndCategory_NoError(t *testing.T) {
+	conn, cleaner := RealDb()
+	defer cleaner("category_tracking", "keyword_tracking", "app_tracking")
+
+	app := NewApp()
+	track := NewTrack()
+	repo := db.New(db.NewAppTracking(conn), inserter_mock{}, db.NewKeywordsTracking(conn), db.NewCategoryTracking(conn))
+
+	id, err := repo.Insert(context.Background(), app)
+	track.BundleId = id
+
+	_, err = repo.Insert(context.Background(), track)
+	assert.NoError(t, err)
+	track.Type = "key"
+	_, err = repo.Insert(context.Background(), track)
+	assert.NoError(t, err)
+}
+
+func TestTrackingDatabaseInsertTx_ShouldInsertInfoInTransaction_NoError(t *testing.T) {
+	conn, cleaner := RealDb()
+	defer cleaner("meta_tracking", "category_tracking", "keyword_tracking")
+
+	app := NewApp()
+	meta := NewMeta()
+	track := NewTrack()
+	repo := db.NewWithConnection(conn)
+
+	id, err := repo.Insert(context.Background(), app)
+	meta.BundleId = id
+	track.BundleId = id
+
+	ctx := context.Background()
+	tx, _ := conn.Begin(ctx)
+
+	_, err = repo.InsertTx(tx, context.Background(), meta)
+	assert.NoError(t, err)
+	_, err = repo.InsertTx(tx, context.Background(), track)
+	assert.NoError(t, err)
+
+	track.Type = "key"
+	_, err = repo.InsertTx(tx, context.Background(), track)
+	assert.NoError(t, err)
+
+	assert.NoError(t, tx.Commit(ctx))
+}
