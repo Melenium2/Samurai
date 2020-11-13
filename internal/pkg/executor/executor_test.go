0f984369c1aa211ae2f15e9bce7ee7958eeaa427
Melenium2 <osumelenium@gmail.com>
2020-11-13 16:20:18 +0500

    Completed main alog + tests

--- internal/pkg/executor/executor_test.go
+++ internal/pkg/executor/executor_test.go
@@ -2,6 +2,7 @@ package executor_test
 
 import (
 	"Samurai/config"
+	"Samurai/internal/pkg/api"
 	"Samurai/internal/pkg/api/inhuman"
 	"Samurai/internal/pkg/api/mobilerpc"
 	"Samurai/internal/pkg/db"
@@ -189,6 +190,288 @@ func TestSamurai_Work_ShouldDoAllWork3Times_NoError(t *testing.T) {
 	assert.Equal(t, 12, len(repo.categorydb))
 	for _, v := range repo.categorydb {
 		assert.Equal(t, int32(2), v.Place)
+	}
+}
+
+func TestSamurai_Done_ShouldStopAfterFirstIteration_NoError(t *testing.T) {
+	c := config.AppConfig{
+		Bundle:   "com.app",
+		Period:   5,
+		Intensity: time.Second * 1,
+		Lang:     "ru_RU",
+		Keywords: []string{"1", "2", "3"},
+	}
+	repo := &mock_repo{
+		appdb:      make(map[int]db.App),
+		metadb:     make(map[int]db.Meta),
+		categorydb: make(map[int]db.Track),
+		keydb:      make(map[int]db.Track),
+	}
+	ex := executor.New(c, mock_api{}, repo)
+
+	go func() {
+		time.Sleep(time.Second * 1)
+		ex.Done()
+	}()
+
+	err := ex.Work()
+	assert.NoError(t, err)
+}
+
+func DatabaseConnection(config config.DBConfig) *pgx.Conn {
+	url, err := db.ConnectionUrl(config)
+	if err != nil {
+		panic(err)
+	}
+	conn, err := db.Connect(url)
+	if err != nil {
+		panic(err)
+	}
+
+	return conn
+}
+
+func TestSamurai_NewApp_ShouldInsertNewRowToDb_NoError(t *testing.T) {
+	c := config.New("../../../config/dev.yml")
+	c.App.Lang = "ru_RU"
+	c.App.Period = 30
+
+	ex := executor.NewDefault(c)
+
+
+	id, err := ex.NewApp(context.Background(), &inhuman.App{
+		Bundle: "com.com.com",
+	})
+
+	assert.NoError(t, err)
+	assert.Greater(t, id, 0)
+
+	conn := DatabaseConnection(c.Database)
+
+	row := conn.QueryRow(context.Background(), "select bundle from app_tracking where id = $1", id)
+	var bundle string
+	assert.NoError(t, row.Scan(&bundle))
+
+	assert.Equal(t, "com.com.com", bundle)
+
+	_, err = conn.Exec(context.Background(), "truncate table app_tracking cascade")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestSamurai_NewApp_ShouldntInsertRowCozContextEmpty_NoError(t *testing.T) {
+	c := config.New("../../../config/dev.yml")
+	c.App.Lang = "ru_RU"
+	c.App.Period = 30
+
+	ex := executor.New(c.App, mock_api{}, db.NewAppTracking(DatabaseConnection(c.Database)))
+
+	assert.Panics(t, func() {
+		ex.NewApp(nil, &inhuman.App{
+			Bundle: "com.com.com",
+		})
+	})
+}
+
+func TestSamurai_UpdateMeta_ShouldInsertNewRow_NoError(t *testing.T) {
+	c := config.New("../../../config/dev.yml")
+	c.App.Lang = "ru_RU"
+	c.App.Period = 30
+
+	conn := DatabaseConnection(c.Database)
+	tr := db.NewWithConnection(conn)
+	ex := executor.New(c.App, mock_api{}, tr)
+
+	app := &inhuman.App{
+		Bundle: "com.com.com",
+		Description: "123",
+	}
+	ctx := context.Background()
+
+	id, err := ex.NewApp(ctx, app)
+	assert.NoError(t, err)
+	ex.TaskId = id
+
+	assert.NoError(t, ex.UpdateMeta(ctx, app))
+
+	row := conn.QueryRow(ctx, "select id from meta_tracking where bundleid = $1", id)
+	var newid int
+	assert.NoError(t, row.Scan(&newid))
+	assert.Greater(t, newid, 0)
+
+	_, err = conn.Exec(context.Background(), "truncate table app_tracking, meta_tracking cascade")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestSamurai_UpdateTrack_ShouldInsertNewKeywordsAndCategories(t *testing.T) {
+	c := config.New("../../../config/dev.yml")
+	c.App.Lang = "ru_RU"
+	c.App.Period = 30
+
+	conn := DatabaseConnection(c.Database)
+	tr := db.NewWithConnection(conn)
+	ex := executor.New(c.App, mock_api{}, tr)
+
+	app := &inhuman.App{
+		Bundle: "com.com.com",
+		Description: "123",
+	}
+	ctx := context.Background()
+
+	id, err := ex.NewApp(ctx, app)
+	assert.NoError(t, err)
+	ex.TaskId = id
+
+	assert.NoError(t, ex.UpdateTrack(ctx, 50, "key"))
+	assert.NoError(t, ex.UpdateTrack(ctx, 50, "finance_apps_top_selling_hello"))
+
+	row := conn.QueryRow(ctx, "select type from keyword_tracking where bundleid = $1", id)
+	var key string
+	assert.NoError(t, row.Scan(&key))
+	assert.Equal(t, "key", key)
+
+	row = conn.QueryRow(ctx, "select type from category_tracking where bundleid = $1", id)
+	var cat string
+	assert.NoError(t, row.Scan(&cat))
+	assert.Equal(t, "finance_apps_top_selling_hello", cat)
+
+	_,  err = conn.Exec(context.Background(), "truncate table app_tracking, keyword_tracking, category_tracking cascade")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestSamurai_Tick_ShouldDoneOnTickOnlyDb(t *testing.T) {
+	c := config.New("../../../config/dev.yml")
+	c.App.Lang = "ru_RU"
+	c.App.Period = 30
+	c.App.Bundle = "com.app"
+	c.App.Keywords = []string {"key1", "key2", "key3"}
+
+	conn := DatabaseConnection(c.Database)
+	tr := db.NewWithConnection(conn)
+	ex := executor.New(c.App, mock_api{}, tr)
+
+	ctx, _ := context.WithTimeout(context.Background(), time.Second * 120)
+
+	assert.NoError(t, ex.Tick(ctx))
+	assert.Greater(t, ex.TaskId, 0)
+
+	row := conn.QueryRow(context.Background(), "select bundle from app_tracking where id = $1", ex.TaskId)
+	var bundle string
+	assert.NoError(t, row.Scan(&bundle))
+	assert.Equal(t, "com.app", bundle)
+
+	row = conn.QueryRow(context.Background(), "select description from meta_tracking where bundleid = $1", ex.TaskId)
+	var desc string
+	assert.NoError(t, row.Scan(&desc))
+	assert.Equal(t, "123", desc)
+
+	rows, err := conn.Query(context.Background(), "select type from keyword_tracking where bundleid = $1", ex.TaskId)
+	assert.NoError(t, err)
+
+	var keywords []string
+	for rows.Next() {
+		var k string
+		assert.NoError(t, rows.Scan(&k))
+		keywords = append(keywords, k)
+	}
+	rows.Close()
+	assert.Equal(t, 3, len(keywords))
+
+	rows, err = conn.Query(context.Background(), "select type from category_tracking where bundleid = $1", ex.TaskId)
+	assert.NoError(t, err)
+
+	var cats []string
+	for rows.Next() {
+		var c string
+		assert.NoError(t, rows.Scan(&c))
+		cats = append(cats, c)
+	}
+	rows.Close()
+	assert.Equal(t, 4, len(cats))
+
+	_, err = conn.Exec(context.Background(), "truncate table app_tracking, keyword_tracking, meta_tracking, category_tracking cascade")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestSamurai_Tick_ShouldDoneOnTick(t *testing.T) {
+	c := config.New("../../../config/dev.yml")
+	c.App.Lang = "ru_RU"
+	c.App.Intensity = time.Hour
+	c.App.Period = 1
+	c.App.Bundle = "com.narvii.amino.x89"
+	c.App.Keywords = []string {"game", "tale", "amino"}
+
+	conn := DatabaseConnection(c.Database)
+	tr := db.NewWithConnection(conn)
+
+	c.Api.GrpcAddress = "localhost"
+	c.Api.GrpcPort = "1000"
+	c.Api.GrpcAccount = &mobilerpc.Account{
+		Login:    "ceciliamcalistervugt93@gmail.com",
+		Password: "Hbibcxzauig",
+		GsfId:    4554796460706948097,
+		Token:    "3QcbjsMii54dJ3NDYGlxLaQbC9HVpspXMkVlZ212GQBezdTiF1kdTIJ16Q80LQeYaJrjOw.",
+		Locale:   "ru_RU",
+		Proxy:    &mobilerpc.Proxy{
+			Http:  "http://STqthJ:2odx6V@45.132.21.233:8000",
+			Https: "https://STqthJ:2odx6V@45.132.21.233:8000",
+		},
+		Device:   "whyred",
+	}
+	request := api.New(c.Api, c.App.Lang)
+
+
+	ex := executor.New(c.App, request, tr)
+
+	ctx, _ := context.WithTimeout(context.Background(), time.Second * 120)
+
+	assert.NoError(t, ex.Tick(ctx))
+	assert.Greater(t, ex.TaskId, 0)
+
+	row := conn.QueryRow(context.Background(), "select bundle from app_tracking where id = $1", ex.TaskId)
+	var bundle string
+	assert.NoError(t, row.Scan(&bundle))
+	assert.Equal(t, c.App.Bundle, bundle)
+
+	row = conn.QueryRow(context.Background(), "select description from meta_tracking where bundleid = $1", ex.TaskId)
+	var desc string
+	assert.NoError(t, row.Scan(&desc))
+	assert.NotEmpty(t, desc)
+
+	rows, err := conn.Query(context.Background(), "select type from keyword_tracking where bundleid = $1", ex.TaskId)
+	assert.NoError(t, err)
+
+	var keywords []string
+	for rows.Next() {
+		var k string
+		assert.NoError(t, rows.Scan(&k))
+		keywords = append(keywords, k)
+	}
+	rows.Close()
+	assert.Equal(t, 3, len(keywords))
+
+	rows, err = conn.Query(context.Background(), "select type from category_tracking where bundleid = $1", ex.TaskId)
+	assert.NoError(t, err)
+
+	var cats []string
+	for rows.Next() {
+		var c string
+		assert.NoError(t, rows.Scan(&c))
+		cats = append(cats, c)
+	}
+	rows.Close()
+	assert.Equal(t, 4, len(cats))
+
+	_, err = conn.Exec(context.Background(), "truncate table app_tracking, keyword_tracking, meta_tracking, category_tracking cascade")
+	if err != nil {
+		panic(err)
 	}
 }
 
